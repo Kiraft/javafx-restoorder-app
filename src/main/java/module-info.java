1b9f70064541b087ddac8dab9181f8dfe349668b
Kiraft <joniosva@gmail.com>
2023-10-08 14:45:04 -0600

    primer commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.application.restoorderapp {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+
+    opens com.application.restoorderapp to javafx.fxml;
+    exports com.application.restoorderapp;
+    exports com.application.restoorderapp.controller;
+    opens com.application.restoorderapp.controller to javafx.fxml;
+}
