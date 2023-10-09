3dc62aa038e2aca79b5ab4c55f59ffeca1e63922
Kiraft <joniosva@gmail.com>
2023-10-08 20:34:51 -0600

    se agrego la parte register en la vista loginandregister

---
+++ src/main/java/com/application/restoorderapp/controller/LoginAndRegisterController.java
@@ -0,0 +1,141 @@
+package com.application.restoorderapp.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+
+public class LoginAndRegisterController {
+
+    @FXML
+    private ComboBox<?> boxCarrera;
+
+    @FXML
+    private Button btnLogin;
+
+    @FXML
+    private Button btnLogin1;
+
+    @FXML
+    private Button btnLogin11;
+
+    @FXML
+    private Button btnRecoverBack;
+
+    @FXML
+    private Button btnRecoverBack1;
+
+    @FXML
+    private Button btnRecoverBack11;
+
+    @FXML
+    private Button btnRecoverBack111;
+
+    @FXML
+    private Button btnRecoverValidate;
+
+    @FXML
+    private Button btnRegister;
+
+    @FXML
+    private Button btnSwitchLogin;
+
+    @FXML
+    private Button btnSwitchRegister;
+
+    @FXML
+    private Button btnSwitchRegister1;
+
+    @FXML
+    private Pane containerLogin;
+
+    @FXML
+    private Pane containerLogin1;
+
+    @FXML
+    private Pane containerLogin11;
+
+    @FXML
+    private Pane containerLogin111;
+
+    @FXML
+    private Pane containerRecoverUser;
+
+    @FXML
+    private Pane containerRegister;
+
+    @FXML
+    private Label labelRecover;
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    private PasswordField txtNewPassword;
+
+    @FXML
+    private TextField txtNombre;
+
+    @FXML
+    private TextField txtNumeroControl;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    private PasswordField txtPassword1;
+
+    @FXML
+    private PasswordField txtPassword11;
+
+    @FXML
+    private TextField txtRecoverEmail;
+
+    @FXML
+    private TextField txtRecoverMatricula;
+
+    @FXML
+    private TextField txtUser;
+
+    @FXML
+    private TextField txtUser1;
+
+    @FXML
+    private TextField txtUser11;
+
+    @FXML
+    void LoginAndRegister(ActionEvent event) {
+
+    }
+
+    @FXML
+    void backLogin(ActionEvent event) {
+
+    }
+
+    @FXML
+    void inOverMouse(MouseEvent event) {
+
+    }
+
+    @FXML
+    void labelRecover(MouseEvent event) {
+
+    }
+
+    @FXML
+    void outOverMouse(MouseEvent event) {
+
+    }
+
+    @FXML
+    void switchForm(ActionEvent event) {
+
+    }
+
+}
