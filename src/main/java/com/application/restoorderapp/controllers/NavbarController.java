8948f3b8a1b834ffff8a0ebe58e6103d060fbe27
Kiraft <77902620+Kiraft@users.noreply.github.com>
2023-10-22 14:37:14 -0600

    Se agrego el identificadores de los componentes y eventos
    A las vistas de la vista Navbar se le agregaron los id a los componentes y los eventos

--- src/main/java/com/application/restoorderapp/controllers/NavbarController.java
+++ src/main/java/com/application/restoorderapp/controllers/NavbarController.java
@@ -1,13 +1,31 @@
 package com.application.restoorderapp.controllers;
 
-
 import com.application.restoorderapp.models.Cuenta;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
 
 public class NavbarController {
+
+    @FXML
+    private Button btnExit;
+
     @FXML
     private Button btnMenu;
+
+    @FXML
+    private Button btnSettings;
+
+    @FXML
+    private Button btnTickets;
+
+    @FXML
+    private AnchorPane containerLeft;
+
+    @FXML
+    private Text labelNombre;
+
     private Cuenta cuenta;
 
     public void setCuenta(Cuenta cuenta) {
@@ -15,4 +33,5 @@ public class NavbarController {
     }
 
 }
+
 
