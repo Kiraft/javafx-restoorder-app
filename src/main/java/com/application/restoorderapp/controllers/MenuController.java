a54aa5455ade2d201e2c4bc8065d033004d609cd
Kiraft <77902620+Kiraft@users.noreply.github.com>
2023-10-25 00:47:08 -0600

    Se agrego una parte de la logica cargar hijos al contendor de productos asiaticos

--- src/main/java/com/application/restoorderapp/controllers/MenuController.java
+++ src/main/java/com/application/restoorderapp/controllers/MenuController.java
@@ -1,11 +1,20 @@
 package com.application.restoorderapp.controllers;
 
+import com.application.restoorderapp.App;
 import com.application.restoorderapp.models.Cuenta;
+import com.application.restoorderapp.models.ElementoMenu;
+import com.application.restoorderapp.models.repositories.ElementoMenuRepositoryImplement;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+import java.util.List;
 
 public class MenuController {
 
@@ -25,14 +34,34 @@ public class MenuController {
     private ScrollPane containerAsiatica;
 
     @FXML
+    private VBox containerAsiaticaNodes;
+
+    @FXML
     private ScrollPane containerCafe;
+
+    @FXML
+    private VBox containerCafeNodes;
 
     @FXML
     private ScrollPane containerInternacional;
 
     @FXML
+    private VBox containerInternacionalNodes;
+
+    @FXML
     private ScrollPane containerPostres;
 
+    @FXML
+    private VBox containerPostresNodes;
+
+    private List<ElementoMenu> internacional;
+    private List<ElementoMenu> postres;
+
+    private List<ElementoMenu> asiatica;
+
+    private List<ElementoMenu> cafe;
+
+    ElementoMenuRepositoryImplement elementoMenuRepositoryImplement = new ElementoMenuRepositoryImplement();
     private Cuenta cuenta;
 
     public void setCuenta(Cuenta cuenta) {
@@ -49,6 +78,26 @@ public class MenuController {
                 break;
             case "btnAsiatica":
                 showAndHiddenMenu(true, false, false, false);
+                loadAsiatica();
+
+
+                containerAsiaticaNodes.getChildren().clear();
+
+                for (ElementoMenu em : asiatica) {
+                    try {
+                        FXMLLoader cardPlatilloLoader = new FXMLLoader(App.class.getResource("view_card_platillo.fxml"));
+                        Parent cardPlatilloRoot = cardPlatilloLoader.load();
+
+                        CardPlatilloController cardPlatilloController = cardPlatilloLoader.getController();
+
+                        cardPlatilloController.setElementoMenu(em);
+
+                        containerAsiaticaNodes.getChildren().add(cardPlatilloRoot);
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
 
                 break;
             case "btnPostres":
@@ -60,6 +109,22 @@ public class MenuController {
             default:
                 break;
         }
+    }
+
+    public void loadInternacional(){
+        internacional = elementoMenuRepositoryImplement.listarPorCategoria("INTERNACIONAL");
+    }
+
+    public void loadAsiatica(){
+        asiatica = elementoMenuRepositoryImplement.listarPorCategoria("ASIATICA");
+    }
+
+    public void loadPostres(){
+        postres = elementoMenuRepositoryImplement.listarPorCategoria("POSTRES");
+    }
+
+    public void loadCafe(){
+        cafe = elementoMenuRepositoryImplement.listarPorCategoria("CAFE");
     }
 
     private void showAndHiddenMenu(boolean asiatica, boolean postre, boolean cafe, boolean internacional) {
