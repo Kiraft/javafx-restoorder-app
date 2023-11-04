78dcc3d446ea97f553caf28758f5738bbc6d5d3c
Kiraft <77902620+Kiraft@users.noreply.github.com>
2023-11-04 14:54:03 -0600

    Se agrego logica para que cada vista tenga informacion de cada orden

--- src/main/java/com/application/restoorderapp/controllers/CardPedidoController.java
+++ src/main/java/com/application/restoorderapp/controllers/CardPedidoController.java
@@ -1,10 +1,21 @@
 package com.application.restoorderapp.controllers;
 
+import com.application.restoorderapp.models.Cuenta;
+import com.application.restoorderapp.models.DetallePedido;
+import com.application.restoorderapp.models.Orden;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
 
-public class CardPedidoController {
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CardPedidoController implements Initializable {
 
     @FXML
     private Label btnCompleted;
@@ -18,10 +29,34 @@ public class CardPedidoController {
     @FXML
     private Label labelIdOrden;
 
+    private Orden orden;
+
+    public void setOrden(Orden orden) {
+        this.orden = orden;
+    }
+
     @FXML
     void completar(MouseEvent event) {
 
     }
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        Thread hilo = new Thread(() -> {
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            Platform.runLater(() -> {
+                labelDate.setText(orden.getFecha().toString());
+                labelIdOrden.getId();
+            });
+        });
+
+        hilo.start();
+    }
 }
 
