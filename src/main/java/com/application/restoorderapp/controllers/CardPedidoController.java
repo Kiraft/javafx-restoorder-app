a45b4b3a7f88cb668c91c3ef9c7ef5b97590a4e6
Kiraft Monroy <jona__cena@hotmail.com>
2023-11-23 07:04:22 -0600

    FInal

--- src/main/java/com/application/restoorderapp/controllers/CardPedidoController.java
+++ src/main/java/com/application/restoorderapp/controllers/CardPedidoController.java
@@ -84,10 +84,12 @@ public class CardPedidoController implements Initializable {
                     label.setStyle("-fx-font-size: 22; -fx-text-fill: black; -fx-font-weight: bold;");
                     label.setText(dp.getCantidad() + " " + dp.getElementoMenu().getNombre());
                     containerDetallesPedido.getChildren().add(label);
-                    Label labelComentario = new Label();
-                    labelComentario.setStyle("-fx-font-size: 22; -fx-text-fill: #959595; -fx-font-weight: bold;");
-                    labelComentario.setText(dp.getComentario());
-                    containerDetallesPedido.getChildren().add(labelComentario);
+                    if(!(dp.getComentario().equals(""))){
+                        Label labelComentario = new Label();
+                        labelComentario.setStyle("-fx-font-size: 22; -fx-text-fill: #959595; -fx-font-weight: regular;");
+                        labelComentario.setText("   --"+dp.getComentario());
+                        containerDetallesPedido.getChildren().add(labelComentario);
+                    }
 
                 }
             });
